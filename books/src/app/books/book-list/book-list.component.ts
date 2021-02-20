1efa81df477f79724ff8c8e549f0c41ad39c735c
Peter Dickten <peter.dickten@dcs-fuerth.de>
2021-02-21 00:21:06 +0100

    initial

---
+++ books/src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'books-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
