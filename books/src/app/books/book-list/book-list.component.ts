d0fa89416c7cabcea5e476bbf77523f9f6c3e2ea
Peter Dickten <peter.dickten@dcs-fuerth.de>
2021-02-21 00:54:11 +0100

    initial

--- books/src/app/books/book-list/book-list.component.ts
+++ books/src/app/books/book-list/book-list.component.ts
@@ -12,19 +12,28 @@ export class BookListComponent implements OnInit {
       isbn: '1234567890',
       title: 'Angular 11 rocks',
       price: 12.99,
-      rating: 4.9
+      rating: 4.9,
+      coverUrl: 'https://m.media-amazon.com/images/I/61+Qbwu03kL._AC_UL320_.jpg'
     },
     {
       isbn: '1234567891',
       title: 'Angular 12 rocks even more',
       price: 13.99,
-      rating: 5.0
+      rating: 5.0,
+      coverUrl: 'https://m.media-amazon.com/images/I/61H1nLnd7rL._AC_UL320_.jpg'
     }
   ];
+
+  imageWidth = 50;
+  imageIsVisible = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleCover() {
+    this.imageIsVisible = !this.imageIsVisible;
   }
 
 }
